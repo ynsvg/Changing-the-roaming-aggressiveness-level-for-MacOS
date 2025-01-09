af5b62e94d663b8aa5c26c4f114940ff71315d39
ynsvg <jboy03007@gmail.com>
2025-01-09 15:55:33 +0500

    first

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+read -p "Хочешь изменить агрессивность(1) или вернуть настройки по умолчанию(2)? (1/2): " par_choise
+
+if [ $par_choise == "1" ]; then
+read -p "Выбери значение агрессивности роуминга Wi-Fi (1-4; 1 - наименьшая агрессивность, 4 - наибольшая): " aggressiveness_level
+
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.airport.preferences AggressivenessLevel -int $aggressiveness_level
+
+sudo ifconfig en0 down
+sudo ifconfig en0 up
+
+echo "Готово!"
+fi
+
+if [ $par_choise == "2" ]; then
+sudo defaults delete /Library/Preferences/SystemConfiguration/com.apple.airport.preferences AggressivenessLevel
+
+sudo ifconfig en0 down
+sudo ifconfig en0 up
+
+echo "Готово!"
+
+fi
+
