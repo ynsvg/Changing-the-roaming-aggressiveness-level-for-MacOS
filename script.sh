c33e69e775ae48efdc561536f342bb51376340ab
ynsvg <jboy03007@gmail.com>
2025-01-21 18:49:07 +0500

    rebase

--- script.sh
+++ script.sh
@@ -29,14 +29,25 @@ PS3='Выберите операцию: '
 select choice in "Изменить агрессивность роуминга" "Вернуть настройки по умолчанию" "Выйти"; do
     case $choice in
         "Изменить агрессивность роуминга") read -p "Выбери значение агрессивности роуминга Wi-Fi (1-4; 1 - наименьшая агрессивность, 4 - наибольшая): " aggressiveness_level
-        sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.airport.preferences AggressivenessLevel -int $aggressiveness_level
+        case $aggressiveness_level in
+        1) sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.airport.preferences AggressivenessLevel -int 1;;
+        2) sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.airport.preferences AggressivenessLevel -int 2;;
+        3) sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.airport.preferences AggressivenessLevel -int 3;;
+        4) sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.airport.preferences AggressivenessLevel -int 4;;
+        *) echo "Введите другое значение!" && continue;;
+        esac
+        
         sudo ifconfig en0 down
         sudo ifconfig en0 up
         echo "Готово!"
         break;;
         
         "Вернуть настройки по умолчанию") sudo defaults delete /Library/Preferences/SystemConfiguration/com.apple.airport.preferences AggressivenessLevel
-        ;;
+        sudo ifconfig en0 down
+        sudo ifconfig en0 up
+        echo "Готово!"
+        break;;
+        
         "Выйти")
         break;;
     
